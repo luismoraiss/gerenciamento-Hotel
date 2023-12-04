0de880d22f6db1378aa0e538019f4f7e96e13fbb
Luis Morais <138785109+luismoraiss@users.noreply.github.com>
2023-12-04 16:54:38 -0300

    jhgfug

---
+++ grafico.js
@@ -0,0 +1,6 @@
+const container = document.getElementById('barra-lateral')
+const barraLateral = document.getElementById('linha')
+
+barraLateral.addEventListener('click', function(){
+    barra-lateral.classList.add('active')
+})
