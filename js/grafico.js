f9da529de3ef6995b98e9835ce0eb076367d4177
Luis Morais <138785109+luismoraiss@users.noreply.github.com>
2023-12-07 07:08:32 -0300

    hgjhk

--- js/grafico.js
+++ js/grafico.js
@@ -1,3 +1,30 @@
+const ctx = document.getElementById('myChart');
 
-// CÓDIGO BARRA LATAERAL!!
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+        datasets: [{
+            label: '# of Votes',
+            data: [205, 130, 145, 117, 183, 220],
+            borderWidth: 1,
 
+
+        }, {
+            label: 'faturamento',
+            data: [12253, 10495, 14739, 9184, 11596, 13831],
+            borderWidth: 1
+        }, {
+           
+            label: 'Hospedagens',
+            data: [1, 2, 45, 7, 92, 2],
+            borderWidth: 1,
+        }]
+    },
+    options: {
+        backgroundColor: '#7c2424',
+
+
+
+    }
+});
